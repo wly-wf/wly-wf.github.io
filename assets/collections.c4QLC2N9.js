b0be035769a69557ef6e6daf997f9da5a8b9ad42
wly-wf <wly-wf@users.noreply.github.com>
2025-10-18 07:36:52 +0000

    deploy: cc8554061552126e339751ab05fd2673782285d8

---
+++ assets/collections.c4QLC2N9.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.DUU2SdDv.js";import{u as l}from"./chunks/vue-router.Brpcmowa.js";import{M as t}from"./framework.BQbPDlGD.js";const e=[];function a(){return{collections:t(()=>e)}}function p(){const o=l(),n=t(()=>o.path.startsWith("/collections/")?o.path.split("/")[2]:"");return{collection:t(()=>e.find(c=>c.key===n.value))}}export{a,p as u};
---
+++ assets/post.CkAGsa5F.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.DUU2SdDv.js";import{o as t}from"./app.rz9DXmMy.js";import{M as e}from"./framework.BQbPDlGD.js";function m(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const s=r.value.types[o].color,i=r.value.types[o].icon,n=e(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:i,styles:n}}export{m as u};
